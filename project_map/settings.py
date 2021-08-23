7adb7a5d3e1b3df3ae5443785c6fede5b6e243cf
flatmass <flatmass1@ya.ru>
2021-08-23 11:44:11 +0300

    Update settings.py

--- project_map/settings.py
+++ project_map/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
         },
         'NAME': 'nov_map_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'itcomsqluser',
-        'PASSWORD': 'CR0504slpot!a',
+        'USER': 'xxxx',
+        'PASSWORD': 'xxxxxxx',
         #'HOST': 'rc1a-jndrbrnu3muvfpme.mdb.yandexcloud.net',
         'HOST': 'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
